6d7d92d6f6fd95ee70acb49fffb71378b1d7c40a
Nathan Shirlberg <nathannis@gmail.com>
2018-12-12 03:50:38 +0000

    Add service configuration and reading of config files

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const app = express()
-const port = 8080
+const config = require('./config.json')
+const port = config.port
+const secrets =  require('./secrets.json')
 
 app.get('/', (req, res) => res.send('Welcome to the garage door manager!'))
 
+console.log(`Secret: ${secrets.secret}`)
 app.listen(port, () => console.log(`Listening on port ${port}!`))
